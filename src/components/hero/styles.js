523f80e819f965bdb7b1f0f30a1a7ba5037abba0
netosilv4 <netosilv44445@gmail.com>
2021-08-16 16:58:39 -0300

    [Neto] Hero

--- src/components/hero/styles.js
+++ src/components/hero/styles.js
@@ -2,10 +2,28 @@ import styled from 'styled-components';
 
 export const Header = styled.header`
   width: 100%;
-  height: 60vh;
+  height: 300px;
   background-image: url('/dale/images/image-hero-mobile.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  padding: 20px 10px;
 `;
 
 export const Logo = styled.img`
-  src: url('dale/images/logo.svg')
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const HamburguerIcon = styled.img`
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  height: 20px;
+  justify-content: space-between;
 `;
