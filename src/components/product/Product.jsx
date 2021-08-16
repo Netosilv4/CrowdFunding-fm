e3042290de098617722daa1e33ea7dd323287cb1
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-08-16 17:08:04 -0300

    Realiza componente Product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import {
+  Logo, Section, H2, P, ButtonBack, Div, Button,
+} from './styles';
 
 const Product = () => (
-  <section>
-    <img src="dale/images/logo-mastercraft.svg" alt="logo mastercraft" />
-    <h2>Mastercraft Bamboo Monitor Riser</h2>
-    <p>A beautifully handcrafted monitor stand to reduce neck and eye strain</p>
-    <div>
-      <button type="button">Back this project</button>
-      <button type="button"><img src="dale/images/icon-bookmark.svg" alt="bookmark icon" /></button>
-    </div>
-  </section>
+  <Section>
+    <Logo src="dale/images/logo-mastercraft.svg" alt="logo mastercraft" />
+    <H2>Mastercraft Bamboo Monitor Riser</H2>
+    <P>A beautifully handcrafted monitor stand to reduce neck and eye strain.</P>
+    <Div>
+      <ButtonBack type="button">Back this project</ButtonBack>
+      <Button type="button"><img src="dale/images/icon-bookmark.svg" alt="bookmark icon" /></Button>
+    </Div>
+  </Section>
 );
 
 export default Product;
--- src/components/product/styles.js
+++ src/components/product/styles.js
@@ -1,5 +1,51 @@
 import styled from 'styled-components';
 
-const Logo = styled.img`
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  justify-content: space-around;
+  align-items: center;
+  text-align: center;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 8px;
+  height: 35vh;
+`;
+
+export const Logo = styled.img`
+  border-radius: 100px;
+  width: 50px;
+`;
+
+export const H2 = styled.h2`
+width: 70%;
+font-size: 20px;
+font-weight: 700;
+`;
+
+export const P = styled.p`
+  color: hsl(0, 0%, 48%);
+`;
+
+export const ButtonBack = styled.button`
+  width: 45vw;
+  padding: 18px;
+  border-radius: 50px;
+  border: 0;
+  background-color: hsl(176, 50%, 47%);
+  color: white;
+  font-weight: 700;
+  margin-right: 8px;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.button`
+  border: 0;
   border-radius: 100px;
 `;
